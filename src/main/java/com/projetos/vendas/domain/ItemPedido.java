32fc20f98028f97a5aeb222f2da5cb9b9caed6a2
RamiroCyber <ramiro.dr@gmail.com>
2021-06-15 11:10:04 -0300

    criando chave estrangeira para item pedido

---
+++ src/main/java/com/projetos/vendas/domain/ItemPedido.java
@@ -0,0 +1,10 @@
+package com.projetos.vendas.domain;
+
+public class ItemPedido {
+
+	private ItemPedidoPK id =  new ItemPedidoPK();
+	
+	private Double preco;
+	private Integer quantidade;
+	private Double desconto;
+}
